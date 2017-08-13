0c65de201433f8aa138d136c455353e2d482b3b3
Ori Arad <aradori@hotmail.com>
2017-08-14 02:16:04 +0300

    SimpleWeather skelaton

---
+++ ideas/SimpleWeather/src/main/java/edu/cse/simplecse/simpleWeather/SimpleWeather.java
@@ -0,0 +1,6 @@
+package edu.cse.simplecse.simpleWeather;
+// This class will provide an interface to get weather data from the national meteorological service
+// for example - here: https://ims.data.gov.il/sites/default/files/observ.xml
+public class SimpleWeather {
+ // TODO
+}
